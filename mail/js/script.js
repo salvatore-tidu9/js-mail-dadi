70cd0ef5bfd5cb4c68cc75396e607dd91e1236cc
Salvatore Tidu <salvatoret9@outlook.it>
2021-02-25 16:35:38 +0100

    JS_Mail_Dadi_(Modifica)

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,26 +1,30 @@
+var user_mail = prompt("Inserisci la tua mail");
+
+console.log(user_mail);
+
 var mail = ["user1@mail.com", "user2@mail.com", "user3@mail.com", "user4@mail.com", "user5@mail.com", "user6@mail.com"];
 
 console.log(mail);
+
+mail.push("user7@mail.com");
 
 
 for (var i = 0; i < mail.length; i++) {
 
     console.log(mail[i]);
 
+    if (user_mail == mail[i]) {
+
+        alert("Accesso consentito !");
+        
+    }
+
+    else if (user_mail != mail[i]) {
+
+        alert("Accesso negato !");
+    }
+
 }
 
 
-var user_mail = prompt("Inserisci la tua mail");
 
-console.log(user_mail);
-
-
-if ((user_mail == "user1@mail.com") || (user_mail == "user2@mail.com") || (user_mail == "user3@mail.com") || (user_mail == "user4@mail.com") || (user_mail == "user5@mail.com") || (user_mail == "user6@mail.com")) {
-    
-    alert("Accesso consentito !");
-}
-
-else if ((user_mail != "user1@mail.com") || (user_mail != "user2@mail.com") || (user_mail != "user3@mail.com") || (user_mail != "user4@mail.com") || (user_mail != "user5@mail.com") || (user_mail != "user6@mail.com")) {
-
-    alert("Accesso negato !");
-}
